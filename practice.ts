09fcda61ed0e4f6b21930b733cac7089839da57d
Devesh Krishan <deveshkrishan2003@gmail.com>
2024-03-24 17:53:06 -0700

    added more notes
    stopped at type assertions

--- practice.ts
+++ practice.ts
@@ -56,3 +56,83 @@ let msg = "hello there!";
 let msg2: string = "hello there!";
 
 // https://www.typescriptlang.org/docs/handbook/2/everyday-types.html
+
+// primitive types are string, number, and boolean
+// can specify array in two ways
+Array<number>;
+function checkArray(numList: number[]){
+
+}
+
+// using keyword any disables all further type checking
+let obj: any = {x: 0}
+
+// remember you can declare type annotations on variable declarations
+let userName: string = "Alice";
+
+// can do optional properties by adding a "?" after the property name
+function printName(obj: {first: string, second?: string}){
+  // ...
+}
+
+printName({first: "Alice"}) // this is fine
+
+// can use type alias 
+type Points = {
+  x: number;
+  y: number;
+}
+
+// and then use type alias in action
+function printCoord(pt: Points) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+// it seems that type alias and interfaces are very similar
+// key difference is that type can be reopeneed to add new properties while interfaces are always extendable
+
+// extending interface example
+interface Animal {
+  name: string;
+}
+
+interface Bear extends Animal {
+  honey: boolean;
+}
+
+// if you declare a bear, it has name and honey properties
+
+// you can also extend type via intersections
+type Animals = {
+  name: string;
+}
+
+type Bears = Animals & {
+  honey: boolean;
+}
+
+// can also get name and honey properties same as interface example
+
+// you can not redeclare types to get new properties
+
+type uno = {
+  title: string;
+}
+
+type uno = {
+  key: number;
+}
+
+// this shows an error called duplicate error compared to interfaces which allows this
+
+interface dos {
+  title: string;
+}
+
+interface dos {
+  key: number;
+}
+
+// no error and you can use both properties
+
